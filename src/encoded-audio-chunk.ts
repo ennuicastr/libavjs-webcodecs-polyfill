d29d927fbe7cb83578959e6a4b2a10d7084aca45
Yahweasel <git-rpo@yahweasel.com>
2024-02-25 17:35:13 -0500

    Updating EncodedAudioChunk (and EncodedVideoChunk) for 20240208 spec

--- src/encoded-audio-chunk.ts
+++ src/encoded-audio-chunk.ts
@@ -3,7 +3,7 @@
  * interface implemented is derived from the W3C standard. No attribution is
  * required when using this library.
  *
- * Copyright (c) 2021 Yahweasel
+ * Copyright (c) 2021-2024 Yahweasel
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted.
@@ -19,20 +19,81 @@
 
 export class EncodedAudioChunk {
     constructor(init: EncodedAudioChunkInit) {
-        this.type = init.type;
-        this.timestamp = init.timestamp;
-        this.duration = init.duration || 0;
-        const data = this._data =
-            new Uint8Array(
+        /* 1. If init.transfer contains more than one reference to the same
+         *    ArrayBuffer, then throw a DataCloneError DOMException. */
+        // 2. For each transferable in init.transfer:
+            /* 1. If [[Detached]] internal slot is true, then throw a
+             *    DataCloneError DOMException. */
+        // (not worth checking in a polyfill)
+
+        /* 3. Let chunk be a new EncodedAudioChunk object, initialized as
+         *    follows */
+        {
+
+            // 1. Assign init.type to [[type]].
+            this.type = init.type;
+
+            // 2. Assign init.timestamp to [[timestamp]].
+            this.timestamp = init.timestamp;
+
+            /* 3. If init.duration exists, assign it to [[duration]], or assign
+             *    null otherwise. */
+            if (typeof init.duration === "number")
+                this.duration = init.duration;
+            else
+                this.duration = null;
+
+            // 4. Assign init.data.byteLength to [[byte length]];
+            this.byteLength = init.data.byteLength;
+
+            /* 5. If init.transfer contains an ArrayBuffer referenced by
+             *    init.data the User Agent MAY choose to: */
+            let transfer = false;
+            if (init.transfer) {
+
+                /* 1. Let resource be a new media resource referencing sample
+                 *    data in init.data. */
+                let inBuffer: ArrayBuffer;
+                if ((<any> init.data).buffer)
+                    inBuffer = (<any> init.data).buffer;
+                else
+                    inBuffer = init.data;
+
+                const t = Array.from(init.transfer);
+                for (const b of t) {
+                    if (b === inBuffer) {
+                        transfer = true;
+                        break;
+                    }
+                }
+            }
+
+            // 6. Otherwise:
+                // 1. Assign a copy of init.data to [[internal data]].
+            
+            const data = new Uint8Array(
                 (<any> init.data).buffer || init.data,
-                (<any> init.data).byteOffset || 0
+                (<any> init.data).byteOffset || 0,
+                (<any> init.data).BYTES_PER_ELEMENT
+                    ? ((<any> init.data).BYTES_PER_ELEMENT * (<any> init.data).length)
+                    : init.data.byteLength
             );
-        this.byteLength = data.byteLength;
+            if (transfer)
+                this._data = data;
+            else
+                this._data = data.slice(0);
+        }
+
+        // 4. For each transferable in init.transfer:
+            // 1. Perform DetachArrayBuffer on transferable
+        // (already done by transferring)
+
+        // 5. Return chunk.
     }
 
     readonly type: EncodedAudioChunkType;
     readonly timestamp: number; // microseconds
-    readonly duration?: number; // microseconds
+    readonly duration: number | null; // microseconds
     readonly byteLength: number;
 
     private _data: Uint8Array;
@@ -53,6 +114,7 @@ export interface EncodedAudioChunkInit {
     timestamp: number; // microseconds
     duration?: number; // microseconds
     data: BufferSource;
+    transfer?: ArrayLike<ArrayBuffer>;
 }
 
 export type EncodedAudioChunkType =
